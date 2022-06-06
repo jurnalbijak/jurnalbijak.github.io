02d64661eab1b50a08cfa38fa3ab224904b864ae
Muhammad Fatkhurrohman <rohmnnn@gmail.com>
2022-06-06 11:04:27 +0700

    add: dist

--- .nuxt/dist/server/server.js
+++ .nuxt/dist/server/server.js
@@ -945,7 +945,7 @@ async function setContext(app, context) {
   // If context not defined, create it
   if (!app.context) {
     app.context = {
-      isStatic: false,
+      isStatic: true,
       isDev: false,
       isHMR: false,
       app,
@@ -2169,6 +2169,10 @@ const layouts = {
 
     isFetching() {
       return this.nbFetching > 0;
+    },
+
+    isPreview() {
+      return Boolean(this.$options.previewData);
     }
 
   },
