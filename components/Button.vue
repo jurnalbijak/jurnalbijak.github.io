365e3f9216ffb3a396f8dcf0c38f87d3f266d5c5
Muhammad Fatkhurrohman <rohmnnn@gmail.com>
2022-06-06 11:15:59 +0700

    change: button to a

--- components/Button.vue
+++ components/Button.vue
@@ -1,11 +1,11 @@
 <template>
-    <button
+    <a
         type="button"
         class="block px-3 py-2 rounded-md text-base font-medium hover:scale-110" :class="[outline ? 'bg-white border border-blue-400 text-blue-400' : 'bg-blue-400 text-white']"
         :href="url"
     >
         {{ text }}
-    </button>
+    </a>
 </template>
 <script>
 export default {
